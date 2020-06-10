7039bf63fda0d3181732f09576465424631d14f9
edumarg <edumarg@gmx.com>
2020-06-10 20:50:47 +0300

    test coneccion server

--- JS/milestone4.js
+++ JS/milestone4.js
@@ -2,15 +2,16 @@ const SERVER_URL = "http://localhost:5050/fibonacci/:number";
 
 let button = document.getElementById("btnCalculator");
 
-function getResultFromServer() {
-    fetch(SERVER_URL)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            const mynumber = document.getElementById("mynumber");
-            const myresult = document.getElementById("myresult");
-            mynumber.value = data.number;
-            myresult.innerText = data.result;
-        });
-}
+fetch(SERVER_URL);
+console
+    .log(fetch(SERVER_URL))
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        const mynumber = document.getElementById("mynumber");
+        const myresult = document.getElementById("myresult");
+        mynumber.value = data.number;
+        myresult.innerText = data.result;
+    });
