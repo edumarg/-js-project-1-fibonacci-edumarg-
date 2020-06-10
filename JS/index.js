116160774df4678370604571f8d76469bbee74b8
edumarg <edumarg@gmx.com>
2020-06-10 10:50:33 +0300

    milestones in funtions

--- JS/index.js
+++ JS/index.js
@@ -3,16 +3,16 @@ function getRndInteger(min, max) {
 }
 
 //-- Milestone 1 - Fibonacci-- //
+function milestone1() {
+    let milestone1Text = document.getElementById("milestone1");
+    let fibonacciIndex = getRndInteger(0, 10);
+    let fibonacciSeq = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
+    milestone1Text.innerText = `The fibonacci of ${fibonacciIndex} is ${fibonacciSeq[fibonacciIndex]}`;
+}
 
-let milestone1Text = document.getElementById("milestone1");
-let fibonacciIndex = getRndInteger(0, 10);
-let fibonacciSeq = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
-milestone1Text.innerText = `The fibonacci of ${fibonacciIndex} is ${fibonacciSeq[fibonacciIndex]}`;
+milestone1();
 
 //--Milestone 2 --//
-milestone2Text = document.getElementById("milestone2");
-fibonacciIndex = getRndInteger(0, 20);
-
 function fibonacci(index) {
     fibonacciSeq = [0, 1];
     for (i = 2; i <= index; i++) {
@@ -20,22 +20,30 @@ function fibonacci(index) {
     }
     return fibonacciSeq[index];
 }
-milestone2Text.innerText = `The fibonacci of ${fibonacciIndex} is ${fibonacci(
-  fibonacciIndex
-)}`;
+
+function milestone2() {
+    milestone2Text = document.getElementById("milestone2");
+    fibonacciIndex = getRndInteger(0, 20);
+    milestone2Text.innerText = `The fibonacci of ${fibonacciIndex} is ${fibonacci(
+    fibonacciIndex
+  )}`;
+}
+
+milestone2();
 
 //--Milestone 3--//
 let milestone3Index = document.getElementById("milestone3Index");
 let milestone3result = document.getElementById("milestone3result");
+let button = document.getElementById("btnCalculator");
 
 console.log("value", fibonacci(milestone3Index.value));
 
-let buttom = document
-    .getElementById("myBbtnCalculator");
+milestone3result.innerText = button.addEventListener(
+    "click",
+    fibonacci(milestone3Index.value)
+);
 
-.addEventListener("click", function() {
-    milestone3result.innerText = fibonacci(milestone3Index.value);
-});
+console.log("result", milestone3result.innerText);
 
 //--Milestone 3.1 Geekout--
 milestone3GeekText = document.getElementById("milestone3Geekout");
