f4807f20bf30daebc38639d2595f1af38d2a8d99
edumarg <edumarg@gmx.com>
2020-06-11 18:05:55 +0300

    added logic for loader milestone 5

--- JS/milestone5.js
+++ JS/milestone5.js
@@ -1,9 +1,12 @@
+const spinner = document.querySelector(".spin");
 const myNumber = document.getElementById("myNumber");
 const myResult = document.getElementById("myResult");
 const SERVER_URL = `http://localhost:5050/fibonacci/${myNumber.value}`;
 const button = document.getElementById("btnCalculator");
 
 button.addEventListener("click", function() {
+    spinner.classList.add("show");
+    myResult.classList.remove("show");
     fetch(SERVER_URL)
         .then(function(response) {
             console.log(response);
@@ -16,4 +19,9 @@ button.addEventListener("click", function() {
         .catch(function(error) {
             console.error("Error:", error);
         });
+});
+
+myResult.addEventListener("load", function() {
+    spinner.classList.remove("show");
+    myResult.classList.add("show");
 });
