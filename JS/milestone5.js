f1c60e3182c06ce0d9c31d0a92ab047dde23ed80
edumarg <edumarg@gmx.com>
2020-06-12 16:16:59 +0300

    finished milestone 5

--- JS/milestone5.js
+++ JS/milestone5.js
@@ -1,27 +1,53 @@
 let myNumber = document.getElementById("myNumber");
 let myResult = document.getElementById("myResult");
 let button = document.getElementById("btnCalculator");
-let load = document.querySelector(".load");
+let load = document.getElementById("load");
+let errorMsg50 = document.getElementById("errorMsg50");
 
 button.addEventListener("click", function() {
-    let SERVER_URL = `http://localhost:5050/fibonacci/${myNumber.value}`;
-    load.style.display = "block";
-    myResult.style.display = "none";
-
-    // load.classList.add("show");
-    // myResult.classList.remove("show");
-    fetch(SERVER_URL)
-        .then(function(response) {
-            console.log(response);
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-            myResult.innerText = data.result;
+    if (myNumber.value > 50) {
+        load.style.display = "none";
+        myResult.style.display = "none";
+        errorMsg50.style.display = "block";
+        errorMsg42.style.display = "none";
+        errorMsg0.style.display = "none";
+    } else if (myNumber.value <= 50) {
+        if (myNumber.value == 42) {
             load.style.display = "none";
-            myResult.style.display = "block";
-        })
-        .catch(function(error) {
-            console.error("Error:", error);
-        });
+            myResult.style.display = "none";
+            errorMsg50.style.display = "none";
+            errorMsg42.style.display = "block";
+            errorMsg0.style.display = "none";
+        } else if (myNumber.value <= 0) {
+            load.style.display = "none";
+            myResult.style.display = "none";
+            errorMsg42.style.display = "none";
+            errorMsg50.style.display = "none";
+            errorMsg0.style.display = "block";
+        } else {
+            let SERVER_URL = `http://localhost:5050/fibonacci/${myNumber.value}`;
+            load.style.display = "block";
+            myResult.style.display = "none";
+            errorMsg42.style.display = "none";
+            errorMsg50.style.display = "none";
+            errorMsg0.style.display = "none";
+            fetch(SERVER_URL)
+                .then(function(response) {
+                    console.log(response);
+                    return response.json();
+                })
+                .catch(function(error) {
+                    console.error(`Error ${myNumber.value}:`, error);
+                })
+                .then(function(data) {
+                    console.log(data);
+                    myResult.innerText = data.result;
+                    load.style.display = "none";
+                    myResult.style.display = "block";
+                    errorMsg42.style.display = "none";
+                    errorMsg50.style.display = "none";
+                    errorMsg0.style.display = "none";
+                });
+        }
+    }
 });
