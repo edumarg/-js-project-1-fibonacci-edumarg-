3b87a5d57cbd8a8b5a18332c65c29c70a5ee7205
edumarg <edumarg@gmx.com>
2020-06-15 13:07:11 +0300

    improved calling results and table and started doing milestone 7.1

--- JS/index.js
+++ JS/index.js
@@ -2,8 +2,10 @@ let myNumber = document.getElementById("myNumber");
 let myResult = document.getElementById("myResult");
 let button = document.getElementById("btnCalculator");
 let load = document.getElementById("load");
+let sortSelect = document.getElementById("sortSelect");
 let errorMsg50 = document.getElementById("errorMsg50");
 let sortedData;
+let resultsTable = document.getElementById("resultsTable");
 let saveCalculationCheckBox = document.getElementById(
     "saveCalculationCheckBox"
 );
@@ -48,24 +50,19 @@ function fetchFibonacciServer() {
     handleLoader("block", "none", "none");
     fetch(SERVER_URL)
         .then(function(response) {
-            console.log(response);
-            console.log(response.status);
             if (response.status === 200) {
                 return response.json();
             }
             if (response.status === 400) {
-                console.log(response.text);
                 return response.text();
             }
         })
         .then(function(data) {
             if (typeof data === "object") {
-                console.log(data);
                 myResult.innerText = data.result;
                 handleLoader("none", "block", "none");
                 myResult.classList.remove("error-msg");
             } else if (typeof data === "string") {
-                console.log(data);
                 myResult.innerText = `Server Error: ${data}`;
                 handleLoader("none", "block", "none");
                 myResult.classList.add("error-msg");
@@ -80,28 +77,59 @@ function callPastResults() {
     let SERVER_URL = "http://localhost:5050/getFibonacciResults";
     fetch(SERVER_URL)
         .then(function(response) {
-            console.log("milestone6", response);
             return response.json();
         })
         .then(function(data) {
             let receivedData = data.results;
-            sortedData = receivedData.sort((a, b) => b.createdDate - a.createdDate);
-            let lastResults = [];
-            for (let i = 0; i < 4; i++) {
-                lastResults.push(
-                    `The Fibonacci of ${sortedData[i].number} is ${
-            sortedData[i].result
-          } Calculated at ${Date(sortedData[i].createdDate)}`
-                );
+            for (let i = 0; i < Object.keys(receivedData).length; i++) {
+                let li = document.createElement("li");
+                li.classList.add("list-group-item");
+                li.innerHTML = `The Fibonacci of <strong>${
+          receivedData[i].number
+        }</strong> is <strong>${
+          receivedData[i].result
+        }</strong> Calculated at ${new Date(receivedData[i].createdDate)}`;
+                resultsTable.appendChild(li);
             }
-            firstLineResults.innerText = lastResults[0];
-            secondLineResults.innerText = lastResults[1];
-            thirdLineResults.innerText = lastResults[2];
-        })
-        .catch(function(error) {
-            console.error(`Error`, error);
         });
+
+    //old function with sort
+    //.then(function(data) {
+    //     let receivedData = data.results;
+    //     sortedData = receivedData.sort((a, b) => b.createdDate - a.createdDate);
+    // let lastResults = [];
+    // for (let i = 0; i < 4; i++) {
+    //         lastResults.push(
+    //             `The Fibonacci of ${sortedData[i].number} is ${
+    //     sortedData[i].result
+    //   } Calculated at ${Date(sortedData[i].createdDate)}`
+    //         );
+    //     }
+    //     firstLineResults.innerText = lastResults[0];
+    //     secondLineResults.innerText = lastResults[1];
+    //     thirdLineResults.innerText = lastResults[2];
+    // })
+    // .catch(function(error) {
+    //     console.error(`Error`, error);
+    // });
+}
+
+function sortedDataFromServer(data) {
+    if (sortSelect.value === 1) {
+        sortedData = data.sort((a, b) => a.number - b.number);
+    } else if (sortSelect === 2) {
+        sortedData = data.sort((a, b) => b.number - a.number);
+    } else if (sortSelect === 3) {
+        sortedData = data.sort((a, b) => a.createdDate - b.createdDate);
+    } else if (sortSelect === 4) {
+        sortedData = data.sort((a, b) => b.createdDate - a.createdDate);
+    } else {
+        sortedData = data;
+    }
+    console.log("sorted data", data);
+    return data;
 }
 
 document.onload = callPastResults();
 button.addEventListener("click", callFibonacciResult);
+// sortSelect.addEventListener("change");
